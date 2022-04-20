3aaa8c3dc13629e4e8e365f4248d663d8ffa4cf4
Teerapong Manakul <Ceece@users.noreply.github.com>
2022-04-20 23:54:41 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  const text = document.querySelector('#text')
+  text.addEventListener('click', function () {
+    window.location.href = 'https://viewer-dev.codebrew.sh/'
+  })
+})
